ecb159022a32f73a7268b190cce36a7ba9267f91
drag0nFISI <cedkvarojas@gmail.com>
2024-11-05 14:19:36 -0500

    en la noche sigo avanzando p

---
+++ src/main/java/Vista/JpanelLogo.java
@@ -0,0 +1,10 @@
+package Vista;
+
+public class JpanelLogo {
+    public static void main(String[] args){
+        Tienda t= new Tienda();
+        t.setVisible(true);
+        t.setLocationRelativeTo(null);
+    }
+    
+}
